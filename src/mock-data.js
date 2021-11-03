8d04a3542d113eae990efc56dfa9da7e82342505
Tiemen Deroose <tiemen.deroose@outlook.be>
2021-11-03 11:42:23 +0100

    Add simple mock data

---
+++ src/mock-data.js
@@ -0,0 +1,37 @@
+const ART_DATA = [
+	{
+        id: '',
+        title: 'Sunlight',
+        description: '',
+        material: 'mixed media',
+        size: 'large',
+        price: 100.0
+	},
+    {
+        id: '',
+        title: 'Autumn Tree',
+        description: '',
+        material: 'acrylic paint',
+        size: 'medium',
+        price: 20.0
+    }
+];
+  
+const JEWELRY_DATA = [
+    { 
+        id: '',
+        name: '', 
+        description: '', 
+        category: 'earring',
+        hue: 'gold',
+    },
+    { 
+        id: '',
+        name: 'Skeletonized petals bracelet', 
+        description: 'A bracelet set with a stone containing skeletonized petals', 
+        category: 'bracelet',
+        hue: 'gold',
+    }
+];
+  
+export {ART_DATA, JEWELRY_DATA};
