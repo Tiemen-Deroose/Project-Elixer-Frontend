2892dfb4fdf99e342233cd31cc28eabfc871a680
Tiemen Deroose <tiemen.deroose@outlook.be>
2021-11-21 14:43:26 +0100

    Update mock data & adapt art component to new data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Art from './components/Art'
-import { ART_DATA } from './mock-data';
+import { ART_DATA } from './data/mock-data';
 
 function App() {
   return (
     <div className="App">
-      {ART_DATA.map(art => <Art title={art.title} description={art.description} material={art.material} size={art.size} price={art.price}/>)}
+      {ART_DATA.map((art, index) => <Art {...art} key={index}/>)}
     </div>
   );
 }
--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,11 +1,16 @@
 export default function Art(props) {
-    const { title, description, material, size, price } = props;
+    const { title, material, medium, size, image_url, price } = props;
     return (
         <div className="bg-green-100 m-4 p-2 w-80">
-            <p className="text-xl font-bold mb-4">{title}</p>
-            <p>{description}</p>
-            <p>{size} painting made with {material}</p>
-            <p>Sold at €{price}</p>
+            <div>
+                <p className="text-xl font-bold">{title}</p>
+                <p className="text-s text-opacity-50 mb-2 italic">({material})</p>
+            </div>
+            <img src={image_url}/>
+            <div>
+                <p>{size} painting made with {medium}</p>
+                <p>Sold at €{price}</p>
+            </div>
         </div>
     );
 }
