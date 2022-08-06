01a572e7bda488da79cdba57b1a0aaf46d691051
Tiemen Deroose <tiemen.deroose@student.hogent.be>
2022-08-06 13:47:57 +0200

    Add update & delete dialog to components

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -4,54 +4,156 @@ import AspectRatioIcon from '@mui/icons-material/AspectRatio';
 import EuroIcon from '@mui/icons-material/Euro';
 import EditIcon from '@mui/icons-material/Edit';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
-import { IconButton, Card, CardActionArea, CardActions, CardContent, CardMedia, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
-import { memo } from 'react';
+import { InputAdornment, IconButton, Card, CardActionArea, CardActions, CardContent, CardMedia, List, ListItem, ListItemIcon, ListItemText, Typography, TextField, Dialog, DialogTitle, DialogContent, Button, DialogActions } from '@mui/material';
+import { memo, useCallback, useState, useContext } from 'react';
 import FavouriteButton from "./FavouriteButton";
+import { ArtContext } from '../contexts/ArtProvider';
 
-export default memo(function Art({ _id, title, material, medium, size, image_url, price, isFavourited, onFavourite }) {
+export default memo(function Art({ _id, title, material, medium, size, image_url, price }) {
+    const { favouriteArt, createOrUpdateArt, deleteArt } = useContext(ArtContext);
+    
+    const [openEditDialog, setOpenEditDialog] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(title);
+    const [editedMaterial, setEditedMaterial] = useState(material);
+    const [editedMedium, setEditedMedium] = useState(medium);
+    const [editedSize, setEditedSize] = useState(size);
+    const [editedImageUrl, setEditedImageUrl] = useState(image_url);
+    const [editedPrice, setEditedPrice] = useState(price);
+
+    const handleFavourite = useCallback(() => {
+        favouriteArt(_id);
+    }, [favouriteArt, _id]);
+
+    const handleDelete = useCallback(() => {
+        deleteArt(_id);
+    }, [deleteArt, _id]);
+
+    const handleEdit = useCallback(() => {
+        createOrUpdateArt({
+            _id,
+            title: editedTitle,
+            material: editedMaterial,
+            medium: editedMedium,
+            size: editedSize,
+            image_url: editedImageUrl,
+            price: editedPrice,
+        });
+        setOpenEditDialog(false);
+    }, [createOrUpdateArt, _id, editedTitle, editedMaterial, editedMedium, editedSize, editedImageUrl, editedPrice]);
+
+    const handleClickEditButton = () => {
+        setOpenEditDialog(true);
+    };
+
     return (
-        <Card className='max-w-xs m-6'>
-            <CardActionArea href={image_url} target='_blank'>
-                <CardMedia
-                    component='img'
-                    height='140'
-                    image={image_url}
-                    alt={title}
-                />
-            </CardActionArea>
-            <CardContent>
-                <Typography variant="h6">
-                    {title}
-                </Typography>
+        <>
+            <Card className='max-w-xs m-6'>
+                <CardActionArea href={image_url} target='_blank'>
+                    <CardMedia className='h-80'
+                        component='img'
+                        image={image_url}
+                        alt={title}
+                    />
+                </CardActionArea>
+                <CardContent>
+                    <Typography variant="h6">
+                        {title}
+                    </Typography>
 
-            </CardContent>
-            <CardContent className='minw-max border-t-2 border-b-2 border-black'>
-                <List>
-                    <ListItem>
-                        <ListItemIcon><PaletteIcon /></ListItemIcon>
-                        <ListItemText primary='Medium' secondary={medium} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemIcon><BrushIcon /></ListItemIcon>
-                        <ListItemText primary='Material' secondary={material} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemIcon><AspectRatioIcon /></ListItemIcon>
-                        <ListItemText primary='Size' secondary={size} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemIcon><EuroIcon /></ListItemIcon>
-                        <ListItemText primary={price} />
-                    </ListItem>
-                </List>
-            </CardContent>
-            <CardActions className='flex justify-between'>
-                <FavouriteButton state={isFavourited} onFavourite={() => onFavourite(_id)} />
-                <div>
-                    <IconButton color='primary' onClick={() => console.log('todo: edit')}><EditIcon fontSize='large' /></IconButton>
-                    <IconButton color='error' onClick={() => console.log('todo: delete')}><RemoveCircleIcon fontSize='large' /></IconButton>
-                </div>
-            </CardActions>
-        </Card>
+                </CardContent>
+                <CardContent className='minw-max border-t-2 border-b-2 border-black'>
+                    <List>
+                        <ListItem>
+                            <ListItemIcon><PaletteIcon /></ListItemIcon>
+                            <ListItemText primary='Medium' secondary={medium} />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon><BrushIcon /></ListItemIcon>
+                            <ListItemText primary='Material' secondary={material} />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon><AspectRatioIcon /></ListItemIcon>
+                            <ListItemText primary='Size' secondary={size} />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon><EuroIcon /></ListItemIcon>
+                            <ListItemText primary={price} />
+                        </ListItem>
+                    </List>
+                </CardContent>
+                <CardActions className='flex justify-between'>
+                    <FavouriteButton initialState={false} onFavourite={() => handleFavourite()} />
+                    <div>
+                        <IconButton color='primary' onClick={handleClickEditButton}><EditIcon fontSize='large' /></IconButton>
+                        <IconButton color='error' onClick={handleDelete}><RemoveCircleIcon fontSize='large' /></IconButton>
+                    </div>
+                </CardActions>
+            </Card>
+
+            <Dialog open={openEditDialog} onClose={handleEdit}>
+                <DialogTitle>Edit Art</DialogTitle>
+                <DialogContent className='space-y-4'>
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        label="Title"
+                        id='title'
+                        value={editedTitle}
+                        onChange={(e) => setEditedTitle(e.target.value)}
+                    />
+
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        label="Image URL"
+                        id='image'
+                        value={editedImageUrl}
+                        onChange={(e) => setEditedImageUrl(e.target.value)}
+                    />
+
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        label="Price"
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">€</InputAdornment>,
+                        }}
+                        id='price'
+                        value={editedPrice}
+                        onChange={(e) => setEditedPrice(e.target.value)}
+                    />
+
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        label="Material"
+                        id='material'
+                        value={editedMaterial}
+                        onChange={(e) => setEditedMaterial(e.target.value)}
+                    />
+
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        label="Medium"
+                        id='medium'
+                        value={editedMedium}
+                        onChange={(e) => setEditedMedium(e.target.value)}
+                    />
+
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        label="Size"
+                        id='size'
+                        value={editedSize}
+                        onChange={(e) => setEditedSize(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleEdit}>Confirm</Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 })
--- src/components/Jewelry.jsx
+++ src/components/Jewelry.jsx
@@ -4,54 +4,156 @@ import PaletteIcon from '@mui/icons-material/Palette';
 import EuroIcon from '@mui/icons-material/Euro';
 import EditIcon from '@mui/icons-material/Edit';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
-import { IconButton, Card, CardActionArea, CardActions, CardContent, CardMedia, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
-import { memo } from 'react';
+import { InputAdornment, IconButton, Card, CardActionArea, CardActions, CardContent, CardMedia, List, ListItem, ListItemIcon, ListItemText, Typography, TextField, Dialog, DialogTitle, DialogContent, Button, DialogActions } from '@mui/material';
+import { memo, useCallback, useState, useContext } from 'react';
 import FavouriteButton from "./FavouriteButton";
+import { JewelryContext } from '../contexts/JewelryProvider';
 
 export default memo(function Jewelry({ _id, name, category, material, colour, image_url, price, isFavourited, onFavourite }) {
-    return (
-        <Card className='max-w-xs m-6'>
-            <CardActionArea href={image_url} target='_blank'>
-                <CardMedia
-                    component='img'
-                    height='140'
-                    image={image_url}
-                    alt={name}
-                />
-            </CardActionArea>
-            <CardContent>
-                <Typography variant="h6">
-                    {name}
-                </Typography>
+    const { favouriteJewelry, createOrUpdateJewelry, deleteJewelry } = useContext(JewelryContext);
 
-            </CardContent>
-            <CardContent className='minw-max border-t-2 border-b-2 border-black'>
-                <List>
-                    <ListItem>
-                        <ListItemIcon><DiamondIcon /></ListItemIcon>
-                        <ListItemText primary='Category' secondary={category} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemIcon><ConstructionIcon /></ListItemIcon>
-                        <ListItemText primary='Material' secondary={material} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemIcon><PaletteIcon /></ListItemIcon>
-                        <ListItemText primary='Colour' secondary={colour} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemIcon><EuroIcon /></ListItemIcon>
-                        <ListItemText primary={price} />
-                    </ListItem>
-                </List>
-            </CardContent>
-            <CardActions className='flex justify-between'>
-                <FavouriteButton state={isFavourited} onFavourite={() => onFavourite(_id)} />
-                <div>
-                    <IconButton color='primary' onClick={() => console.log('todo: edit')}><EditIcon fontSize='large' /></IconButton>
-                    <IconButton color='error' onClick={() => console.log('todo: delete')}><RemoveCircleIcon fontSize='large' /></IconButton>
-                </div>
-            </CardActions>
-        </Card>
+    const [openEditDialog, setOpenEditDialog] = useState(false);
+    const [editedName, setEditedName] = useState(name);
+    const [editedCategory, setEditedCategory] = useState(category);
+    const [editedMaterial, setEditedMaterial] = useState(material);
+    const [editedColour, setEditedColour] = useState(colour);
+    const [editedImageUrl, setEditedImageUrl] = useState(image_url);
+    const [editedPrice, setEditedPrice] = useState(price);
+
+    const handleFavourite = useCallback(() => {
+        favouriteJewelry(_id);
+    }, [favouriteJewelry, _id]);
+
+    const handleDelete = useCallback(() => {
+        deleteJewelry(_id);
+    }, [deleteJewelry, _id]);
+
+    const handleEdit = useCallback(() => {
+        createOrUpdateJewelry({
+            _id,
+            name: editedName,
+            category: editedCategory,
+            material: editedMaterial,
+            colour: editedColour,
+            image_url: editedImageUrl,
+            price: editedPrice,
+        });
+        setOpenEditDialog(false);
+    }, [createOrUpdateJewelry, _id, editedName, editedCategory, editedMaterial, editedColour, editedImageUrl, editedPrice]);
+
+    const handleClickEditButton = () => {
+        setOpenEditDialog(true);
+    };
+
+    return (
+        <>
+            <Card className='max-w-xs m-6'>
+                <CardActionArea href={image_url} target='_blank'>
+                    <CardMedia className='h-80'
+                        component='img'
+                        image={image_url}
+                        alt={name}
+                    />
+                </CardActionArea>
+                <CardContent>
+                    <Typography variant="h6">
+                        {name}
+                    </Typography>
+
+                </CardContent>
+                <CardContent className='minw-max border-t-2 border-b-2 border-black'>
+                    <List>
+                        <ListItem>
+                            <ListItemIcon><DiamondIcon /></ListItemIcon>
+                            <ListItemText primary='Category' secondary={category} />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon><ConstructionIcon /></ListItemIcon>
+                            <ListItemText primary='Material' secondary={material} />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon><PaletteIcon /></ListItemIcon>
+                            <ListItemText primary='Colour' secondary={colour} />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon><EuroIcon /></ListItemIcon>
+                            <ListItemText primary={price} />
+                        </ListItem>
+                    </List>
+                </CardContent>
+                <CardActions className='flex justify-between'>
+                    <FavouriteButton state={isFavourited} onFavourite={() => onFavourite(_id)} />
+                    <div>
+                        <IconButton color='primary' onClick={handleClickEditButton}><EditIcon fontSize='large' /></IconButton>
+                        <IconButton color='error' onClick={handleDelete}><RemoveCircleIcon fontSize='large' /></IconButton>
+                    </div>
+                </CardActions>
+            </Card>
+
+            <Dialog open={openEditDialog} onClose={handleEdit}>
+                <DialogTitle>Edit Jewelry</DialogTitle>
+                <DialogContent className='space-y-4'>
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        label="Name"
+                        id='name'
+                        value={editedName}
+                        onChange={(e) => setEditedName(e.target.value)}
+                    />
+
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        label="Image URL"
+                        id='image'
+                        value={editedImageUrl}
+                        onChange={(e) => setEditedImageUrl(e.target.value)}
+                    />
+
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        label="Price"
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">€</InputAdornment>,
+                        }}
+                        id='price'
+                        value={editedPrice}
+                        onChange={(e) => setEditedPrice(e.target.value)}
+                    />
+
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        label="Category"
+                        id='category'
+                        value={editedCategory}
+                        onChange={(e) => setEditedCategory(e.target.value)}
+                    />
+
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        label="Material"
+                        id='material'
+                        value={editedMaterial}
+                        onChange={(e) => setEditedMaterial(e.target.value)}
+                    />
+
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        label="Colour"
+                        id='colour'
+                        value={editedColour}
+                        onChange={(e) => setEditedColour(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleEdit}>Confirm</Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 })
