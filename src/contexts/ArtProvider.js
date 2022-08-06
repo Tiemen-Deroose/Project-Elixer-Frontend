80eb267f3ae3e6b57afb429256709c035c9eae0b
Tiemen Deroose <tiemen.deroose@student.hogent.be>
2022-08-06 13:46:56 +0200

    Add update & delete operations to providers

--- src/contexts/ArtProvider.js
+++ src/contexts/ArtProvider.js
@@ -8,33 +8,11 @@ export const ArtProvider = ({ children }) => {
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
 
-  const createArt = async ({
-    title,
-    material,
-    medium,
-    size,
-    image_url,
-    price,
-  }) => {
-    setError();
-    try {
-      const { newArt } = await axios.post('http://localhost:9000/api/art/',
-        { title, material, medium, size, image_url, price: Number(price), });
-      await refreshArt();
-      return newArt;
-    } catch (error) {
-      console.error(error);
-      setError(error);
-    }
-  };
-  
   const refreshArt = useCallback(async () => {
     try {
       setError();
       setLoading(true);
-      const { data } = await axios.get(
-        `http://localhost:9000/api/art?limit=10&offset=0`
-      );
+      const { data } = await axios.get(`http://localhost:9000/api/art?limit=10&offset=0`);
       setArtList(data.data);
     } catch (error) {
       setError(error)
@@ -43,12 +21,50 @@ export const ArtProvider = ({ children }) => {
     }
   }, []);
 
+  const createOrUpdateArt = useCallback(async ({ _id, title, material, medium, size, image_url, price }) => {
+    setError();
+    setLoading(true);
+
+    const method = _id ? 'put' : 'post';
+    const url = `http://localhost:9000/api/art/${_id}`;
+    const data = { title, material, medium, size, image_url, price: Number(price) };
+
+    try {
+      const { resultArt } = await axios({ method, url, data });
+      await refreshArt();
+      return resultArt;
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }, [refreshArt]);
+
+  const deleteArt = useCallback(async (_id) => {
+    setError();
+    setLoading(true);
+
+    const url = `http://localhost:9000/api/art/${_id}`;
+
+    try {
+      const { result } = await axios.delete(url);
+      await refreshArt();
+      return result;
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }, [refreshArt]);
+
   useEffect(() => {
       refreshArt();
   }, [refreshArt]);
 
   return (
-    <ArtContext.Provider value={{ artList, error, loading, createArt}} key={'123123'}>
+    <ArtContext.Provider value={{ artList, error, loading, createOrUpdateArt, deleteArt}}>
       {children}
     </ArtContext.Provider>
   );
--- src/contexts/JewelryProvider.js
+++ src/contexts/JewelryProvider.js
@@ -7,34 +7,12 @@ export const JewelryProvider = ({ children }) => {
   const [jewelryList, setJewelryList] = useState([]);
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
-
-  const createJewelry = async ({
-    name, 
-    category, 
-    material, 
-    colour, 
-    image_url, 
-    price,
-  }) => {
-    setError();
-    try {
-      const { newJewelry } = await axios.post('http://localhost:9000/api/jewelry/',
-        { name, category, material, colour, image_url, price: Number(price), });
-      await refreshJewelry();
-      return newJewelry;
-    } catch (error) {
-      console.error(error);
-      setError(error);
-    }
-  };
   
   const refreshJewelry = useCallback(async () => {
     try {
       setError();
       setLoading(true);
-      const { data } = await axios.get(
-        `http://localhost:9000/api/jewelry?limit=10&offset=0`
-      );
+      const { data } = await axios.get(`http://localhost:9000/api/jewelry?limit=10&offset=0`);
       setJewelryList(data.data);
     } catch (error) {
       setError(error)
@@ -42,13 +20,51 @@ export const JewelryProvider = ({ children }) => {
       setLoading(false);
     }
   }, []);
+  
+  const createOrUpdateJewelry = useCallback(async ({ _id, name, category, material, colour, image_url, price }) => {
+    setError();
+    setLoading(true);
+
+    const method = _id ? 'put' : 'post';
+    const url = `http://localhost:9000/api/jewelry/${_id}`;
+    const data = { name, category, material, colour, image_url, price: Number(price) };
+
+    try {
+      const { resultJewelry } = await axios({ method, url, data });
+      await refreshJewelry();
+      return resultJewelry;
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }, [refreshJewelry]);
+
+  const deleteJewelry = useCallback(async (_id) => {
+    setError();
+    setLoading(true);
+
+    const url = `http://localhost:9000/api/jewelry/${_id}`;
+
+    try {
+      const { result } = await axios.delete(url);
+      await refreshJewelry();
+      return result;
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }, [refreshJewelry]);
 
   useEffect(() => {
       refreshJewelry();
   }, [refreshJewelry]);
 
   return (
-    <JewelryContext.Provider value={{ jewelryList, error, loading, createJewelry}}>
+    <JewelryContext.Provider value={{ jewelryList, error, loading, createOrUpdateJewelry, deleteJewelry}}>
       {children}
     </JewelryContext.Provider>
   );
