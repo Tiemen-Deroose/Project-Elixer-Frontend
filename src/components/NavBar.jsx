8259e1936cf275f844394b3de27bddfa8c0f2dee
Tiemen Deroose <tiemen.deroose@student.hogent.be>
2022-08-06 15:23:35 +0200

    Add NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return <>
+        <AppBar position='static'>
+            <Toolbar className='flex justify-between'>
+                <div className='space-x-8'>
+                    <Link to="/">
+                        Browse
+                    </Link>
+                    <Link to="/art">
+                        Art
+                    </Link>
+                    <Link to="/jewelry">
+                        Jewelry
+                    </Link>
+                </div>
+            </Toolbar>
+        </AppBar>
+    </>
+}
+export default Navbar;
