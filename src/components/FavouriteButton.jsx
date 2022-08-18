420f2650910867882a0dd537669bbe73a7ad3e67
Tiemen Deroose <tiemen.deroose@student.hogent.be>
2022-08-18 19:21:13 +0200

    Add UseCallback to chooseEmoji

--- src/components/FavouriteButton.jsx
+++ src/components/FavouriteButton.jsx
@@ -2,10 +2,6 @@ import React, { useCallback } from 'react';
 import { IconButton } from '@mui/material';
 import { Favorite, FavoriteBorder } from '@mui/icons-material';
 
-const chooseEmoji = (state) => {
-  return state ? <Favorite fontSize='large' data-cy='unfavourite_icon' /> : <FavoriteBorder fontSize='large' data-cy='favourite_icon' />;
-};
-
 export default function FavouriteButton({ initialState, onFavourite }) {
   const [state, setState] = React.useState(initialState);
   const [disabled, setDisabled] = React.useState(false);
@@ -22,6 +18,10 @@ export default function FavouriteButton({ initialState, onFavourite }) {
       onFavourite();
     }
   }, [disableButton, disabled, onFavourite, state]);
+
+  const chooseEmoji = useCallback((state) => {
+    return state ? <Favorite fontSize='large' data-cy='unfavourite_icon' /> : <FavoriteBorder fontSize='large' data-cy='favourite_icon' />;
+  }, [state]);
 
   return <IconButton color='error' onClick={toggleState} data-cy='favourite_button'>
     {chooseEmoji(state)}
